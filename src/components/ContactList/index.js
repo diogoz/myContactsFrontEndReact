185270e0ac8474bc7e8e6ae529ab832e9257f8d5
Diogo Sousa <dhesousa77@gmail.com>
2023-07-04 23:10:14 -0300

    Criação da listagem de contatos

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -1,4 +1,9 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactList() {
   return (
@@ -7,6 +12,57 @@ export default function ContactList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diogo Sousa</strong>
+              <small>instagram</small>
+            </div>
+            <span>diogo@diogo.com</span>
+            <span>(18)9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit contact" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete contact" />
+            </button>
+          </div>
+
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Diogo Sousa</strong>
+              <small>instagram</small>
+            </div>
+            <span>diogo@diogo.com</span>
+            <span>(18)9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit contact" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete contact" />
+            </button>
+          </div>
+
+        </Card>
+      </ListContainer>
     </Container>
   );
 }

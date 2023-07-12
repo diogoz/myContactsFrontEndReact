0aa7895d1f81ac0aa678782a5b513804472f4f1a
Diogo Sousa <dhesousa77@gmail.com>
2023-07-12 16:13:20 -0300

    Criacao dos componentes input e select

---
+++ src/components/Select.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  height: 52px;
+  padding: 0 16px;
+  font-size: 16px;
+  border: 2px solid #fff;
+  background: #FFF;
+  border-radius: 4px;
+  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04);
+  outline: none;
+  transition: border-color 0.2s ease-in;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;

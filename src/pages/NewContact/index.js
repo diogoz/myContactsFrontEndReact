5b22208673bc8b5266e45b767bd5c2a9e6799835
Diogo Sousa <dhesousa77@gmail.com>
2023-07-12 17:16:57 -0300

    Criacao do componente button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -11,6 +12,9 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Linkedin</option>
       </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
     </>
   );
 }
